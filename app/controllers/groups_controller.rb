8aeb41c7608ad6685181a351d020643d46687e1a
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-09-11 15:39:57 +0800

    create groupsgroups index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,44 +1,5 @@
 class GroupsController < ApplicationController
-def index
-  @groups = Group.all
-end
-
-def show
-  @group = Group.find(params[:id])
-end
-
-def edit
-  @group = Group.find(params[:id])
-end
-def new
-  @group = Group.new
-end
-
-def create
-  @group = Group.new(group_params)
-  @group.save
-
-  redirect_to groups_path
-end
-
-def update
-  @group =Group.find(params[:id])
-
-  @group.update(group_params)
-
-  redirect_to groups_path, notice: "Update Success"
-end
-
-def destroy
-  @group = Group.find(params[:id])
-  @group.destroy
-  flash[:alert] = "Group deleted"
-  redirect_to groups_path
-end
-
-private
-
-def group_params
-  params.require(:group).permit(:title, :description)
-end
+  def index
+    @groups = Group.all
+  end
 end
