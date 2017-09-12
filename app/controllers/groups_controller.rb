a08da559890199ae7c0ba5af4502d71550ee6081
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-09-12 16:40:24 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,10 +17,14 @@ end
 
   def create
     @group = Group.new(group_params)
-    @group.save
+  if @group.save
 
     redirect_to groups_path
+else
+  render :new
+end
   end
+
 
 def update
   @group = Group.find(params[:id])
