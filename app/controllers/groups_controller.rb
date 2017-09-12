9fc39c38401bf27ceaf95c62e88783d143f65c8a
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-09-12 16:45:11 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,10 +28,14 @@ end
 
 def update
   @group = Group.find(params[:id])
+if @group.update(group_params)
 
-  @group.update(group_params)
 
   redirect_to groups_path, notice: "Update Success"
+else
+  render :edit
+end
+
 end
 
 def destroy
